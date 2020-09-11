401a404ef3792943ea330ad8ddd68f00a39028cd
Vinicius Bittencourt <vinisataides@gmail.com>
2020-09-11 19:31:40 -0300

    Login - Register

--- Frontend/web/src/pages/login/index.tsx
+++ Frontend/web/src/pages/login/index.tsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import bigimg from "../../assets/happy-bunch.png";
+
+import "./styles.css";
 
 function login() {
     return (
-        <div className="container">
-            <h1>Hello World</h1>
+        <div className="bigcontainer">
+            <div className="left_container">
+                <form className="form">
+                    <h1>Register.</h1>
+                    <p>
+                        Enter your information to create your personal account.
+                    </p>
+                    <div className="group-form">
+                        <label htmlFor="email">Your e-mail</label>
+                        <input type="email" name="email" />
+                    </div>
+                    <div className="group-form">
+                        <label htmlFor="pass">Password</label>
+                        <input type="password" name="pass" />
+                    </div>
+                    <button type="submit">Register</button>
+                </form>
+                <Link className="signup" to="/register">
+                    Don't have an account? <span>Sign up</span>
+                </Link>
+            </div>
+            <div className="right_container">
+                <img src={bigimg} alt="logo" />
+                <h1>Register Now</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                    diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                    aliquyam.
+                </p>
+            </div>
         </div>
     );
 }
--- Frontend/web/src/pages/register/index.tsx
+++ Frontend/web/src/pages/register/index.tsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiLogOut } from "react-icons/fi";
+
+import bigimg from "../../assets/happy-bunch.png";
 
 function register() {
     return (
-        <div className="container">
-            <h1>Register</h1>
+        <div className="bigcontainer">
+            <div className="left_container">
+                <form className="form">
+                    <h1>Log in.</h1>
+                    <p>
+                        log in with your information entered during your
+                        registrations.
+                    </p>
+                    <div className="group-form">
+                        <label htmlFor="email">Your e-mail</label>
+                        <input type="email" name="email" />
+                    </div>
+                    <div className="group-form">
+                        <label htmlFor="pass">Password</label>
+                        <input type="password" name="pass" />
+                    </div>
+                    <button type="submit">Log in</button>
+                </form>
+                <Link className="signup" to="/register">
+                    Don't have an account? <FiLogOut />
+                </Link>
+            </div>
+            <div className="right_container">
+                <img src={bigimg} alt="logo" />
+                <h1>Welcome to Event Manager</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                    diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                    aliquyam.
+                </p>
+            </div>
         </div>
     );
 }
